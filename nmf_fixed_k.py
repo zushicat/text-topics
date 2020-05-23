c9ca29a7f3dc04768f3fe931685abdd5167b007b
zushicat <karinhoehne@akasushi.de>
2020-05-23 17:10:08 +0200

    filter unknwon topic by threshold for topic distr.

--- nmf_fixed_k.py
+++ nmf_fixed_k.py
@@ -13,16 +13,24 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 def _train_nmf(n_topics: int, vector_matrix: Any) -> Any:
+    '''
+    For further information about NMF hyperparameter, please refer:
+    https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html
+    '''
     nmf = NMF(n_components=n_topics, init="nndsvd").fit(vector_matrix)
     return nmf
 
 
 def _get_topic_top_words(nmf: Any, n_top_words: int) -> Dict[str, str]:
-        topics = {}
-        for topic_idx, topic in enumerate(nmf.components_):
-            topics[f"topics_{topic_idx}"] = " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+    '''
+    This is a little different than the same named funktion in nmf_unknown_k.py: 
+    returns dict with topic index: string of top words (instead of list of lists with top words)
+    '''
+    topics = {}
+    for topic_idx, topic in enumerate(nmf.components_):
+        topics[f"topics_{topic_idx}"] = " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
 
-        return topics
+    return topics
 
 
 if __name__ == '__main__':
@@ -38,13 +46,13 @@ if __name__ == '__main__':
     nmf_model = _train_nmf(n_topics, vector_matrix)
 
     # ***
-    # explore topics and create human readable topic name list
+    # explore topics and create human readable topic name list; add attribute for unknown topics (see prediction below)
     # ! this hard coded list depends on input and is only fixed as long as nothing changes !
     n_top_words = 15
     topics_top_words: Dict[str, str] = _get_topic_top_words(nmf_model, n_top_words)
 
     # print(json.dumps(topics_top_words, indent=2))
-    topic_names: List[str] = ["bio_tudor", "bio_silent_movie_stars", "bio_design_arch"]
+    topic_names: List[str] = ["bio_tudor", "bio_silent_movie_stars", "bio_design_arch", "unknown_topic"]
 
     # ***
     # check out source text topics
@@ -63,13 +71,23 @@ if __name__ == '__main__':
     # predict topics of short test texts
     titles_by_topics: Dict[str, List[str]] = {x:[] for x in topic_names}  # re-init
     
+    # there is also 1 text about "Charlie Brown" (wikipedia_d_1.txt) which belongs to the unknown topic "peanuts"
+    # Hence, define a threshold for the topic destribution: below this value text topic is unknwon
+    threshold = 0.1
+    
     target_texts = get_texts("target_texts")  # get short test texts
     pred_vector_matrix = vectorizer.transform_documents_to_vectormatrix(target_texts.values())  # get vector matrix of new texts with fitted vectorizer
     pred_y = nmf_model.transform(pred_vector_matrix)  # get topic probabiliy
     for i, p in enumerate(pred_y):  # same as above
         doc_title = list(target_texts.keys())[i]
-        predicted_topic_index = np.argmax(p)
-        topic_name = topic_names[predicted_topic_index]
+        
+        # print(doc_title, p)  # see topic distribution for each text
+        
+        predicted_topic_index = np.argmax(p)  # get index of max. value
+        if p[predicted_topic_index] < threshold:
+            topic_name = "unknown_topic"
+        else:
+            topic_name = topic_names[predicted_topic_index]
         titles_by_topics[topic_name].append(doc_title)
     
     print(json.dumps(titles_by_topics, indent=2))
--- nmf_unknown_k.py
+++ nmf_unknown_k.py
@@ -15,16 +15,24 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 def _train_nmf(n_topics: int, vector_matrix: Any) -> Any:
+    '''
+    For further information about NMF hyperparameter, please refer:
+    https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html
+    '''
     nmf = NMF(n_components=n_topics, init="nndsvd").fit(vector_matrix)
     return nmf
 
 
 def _get_topics_top_words(nmf_model: Any, feature_names: List[str], n_top_words: int) -> List[List[str]]:
-        topics = []
-        for _, topic in enumerate(nmf_model.components_):
-            topics.append([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+    '''
+    This is a little different than the same named funktion in nmf_fixed_k.py: 
+    returns list of lists with top words (instead of dict with strings)
+    '''
+    topics = []
+    for _, topic in enumerate(nmf_model.components_):
+        topics.append([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
 
-        return topics
+    return topics
 
 
 def _get_topic_coherence(word_2_vec_lookup: Any, topics_words: List[List[str]]) -> float:
@@ -48,7 +56,8 @@ def _get_best_k(doc_texts: Set[str], vector_matrix: Any, kmin: int, kmax: int, n
     word_2_vec_lookup = word_2_vec_model.wv
 
     # ***
-    # 
+    # For each number of topics (k):
+    # get the best topic coherence of word_2_vec_lookup and top words in topics
     k_values = []
     topic_coherences: Dict[str, float] = {}
     for k in range(kmin, kmax+1):
@@ -89,11 +98,11 @@ if __name__ == '__main__':
     nmf_model = _train_nmf(n_topics, vector_matrix)
 
     # ***
-    # explore topics and create human readable topic name list
+    # explore topics and create human readable topic name list; add attribute for unknown topics (see prediction below)
     # ! this hard coded list depends on input and is only fixed as long as nothing changes !
     topics_top_words: Dict[str, str] = _get_topics_top_words(nmf_model, feature_names, n_top_words)
     # print(json.dumps([" ".join(x) for x in topics_top_words], indent=2))
-    topic_names: List[str] = ["bio_tudor", "bio_silent_movie_stars", "bio_design_arch"]
+    topic_names: List[str] = ["bio_tudor", "bio_silent_movie_stars", "bio_design_arch", "unknown_topic"]
     
 
     # ***
@@ -112,14 +121,24 @@ if __name__ == '__main__':
     # ***
     # predict topics of short test texts
     titles_by_topics: Dict[str, List[str]] = {x:[] for x in topic_names}  # re-init
+
+    # there is also 1 text about Charlie Brown (wikipedia_d_1.txt) which belongs to the unknown topic "peanuts"
+    # Hence, define a threshold for the topic destribution: below this value is unknwon
+    threshold = 0.1
     
     target_texts = get_texts("target_texts")  # get short test texts
     pred_vector_matrix = vectorizer.transform_documents_to_vectormatrix(target_texts.values())  # get vector matrix of new texts with fitted vectorizer
     pred_y = nmf_model.transform(pred_vector_matrix)  # get topic probabiliy
     for i, p in enumerate(pred_y):  # same as above
         doc_title = list(target_texts.keys())[i]
-        predicted_topic_index = np.argmax(p)
-        topic_name = topic_names[predicted_topic_index]
+        
+        # print(doc_title, p)  # see topic distribution for each text
+        
+        predicted_topic_index = np.argmax(p)  # get index of max. value
+        if p[predicted_topic_index] < threshold:
+            topic_name = "unknown_topic"
+        else:
+            topic_name = topic_names[predicted_topic_index]
         titles_by_topics[topic_name].append(doc_title)
     
     print(json.dumps(titles_by_topics, indent=2))
